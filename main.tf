d68f5e07d477bcf883d5e9a9142a85010e1fc110
Steve Brown <stephen.brown@konghq.com>
2022-10-14 22:02:12 +0100

    add tls key to the module

--- main.tf
+++ main.tf
@@ -33,6 +33,11 @@ locals {
   ]
 }
 
+resource "tls_private_key" "main" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P256"
+}
+
 resource "kubernetes_namespace" "flux_system" {
   metadata {
     name = "flux-system"
