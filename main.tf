503f96480f37385cf713ea34a5fdac4d595a8743
Steve Brown <stephen.brown@konghq.com>
2022-10-14 22:00:08 +0100

    add namespace creation to the module

--- main.tf
+++ main.tf
@@ -33,6 +33,18 @@ locals {
   ]
 }
 
+resource "kubernetes_namespace" "flux_system" {
+  metadata {
+    name = "flux-system"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+    ]
+  }
+}
+
 resource "kubectl_manifest" "install" {
   for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [kubernetes_namespace.flux_system]
